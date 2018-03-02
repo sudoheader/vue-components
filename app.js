c8ae2cd9d2d388924f6dc16a42e13051927caaa4
Raymond Nazaryan <rayn2427@gmail.com>
2018-03-01 21:50:37 -0800

    Added data method to store for component

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+// var data = { status: 'Critical' };
+
 Vue.component('my-cmp', {
     data: function() {
         return {
             status: 'Critical'
-        }
+        };
     },
-    template: '<p>Server Status: {{ status }}</p>'
+    template: '<p>Server Status: {{ status }} (<buttion @click="changeStatus">Change</buttion>) </p>',
+    methods: {
+        changeStatus: function () {
+            this.status = 'Normal';
+        }
+    }
 });
 
 new Vue({
